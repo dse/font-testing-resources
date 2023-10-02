298bb9f1def2a7d275c2af7d87da2979e3f243e6
Darren Embry <dse@webonastick.com>
2023-10-01 22:27:55 -0400

    Fire! Branch fire-master-dse@webonastick.com-1696213675.

---
+++ coding40.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <math.h>
+#define R1(a,b,c)\
+  (-1f*a+sqrt(1f*b*b-4f*a*c))/2f/a)
+#define R2(a,b,c)\
+  (-1f*a-sqrt(1f*b*b-4f*a*c))/2f/a)
+int main(int argc, char** argv) {
+    printf("Hello, %s!\n", "world");
+    int a = 1, b = 2, c = 1;
+    printf("%d\n",
+}
